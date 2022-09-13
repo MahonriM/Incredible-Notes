4a027bb2037535f28be2f771e8dbe36d52fda114
Mahonri Martinez <cripto3000z@gmail.com>
2022-09-13 01:58:41 -0500

    Adding the files by MahonriM

---
+++ app/src/main/java/com/example/notes/NoteActivity.java
@@ -0,0 +1,24 @@
+package com.example.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class NoteActivity extends AppCompatActivity {
+    private EditText editText;
+    private int id;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+        //loading the note
+        editText=findViewById(R.id.note_edit_text);
+        String contents=getIntent().getStringExtra("contents");
+        id=getIntent().getIntExtra("id",0);
+        editText.setText(contents); }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        MainActivity.database.noteDAo().save(editText.getText().toString(),id); }
+}
